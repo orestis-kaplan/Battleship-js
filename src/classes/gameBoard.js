b4bf22d3c18fca290fe4cbe9891569c76c71c299
stickler-ci <support@stickler-ci.com>
2019-09-11 17:09:46 +0000

    Fixing style errors.

--- src/classes/gameBoard.js
+++ src/classes/gameBoard.js
@@ -1,6 +1,6 @@
 function initMap(size) {
   const map = Array.from(Array(size), (_element, row) => Array.from(Array(size), (_ele, column) => (
-      { x: row, y: column, occupied: null })));
+    { x: row, y: column, occupied: null })));
   return map;
 }
 
